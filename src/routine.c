060c425ae3b9921126d3931bc368c59777b8ffe0
Felix Quehl <Felix.Quehl@icloud.com>
2020-11-28 15:45:46 +0100

    added simple logging macro

--- src/routine.c
+++ src/routine.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "routine.h"
 #include "feature.h"
+#include "logging.h"
 
 int routine(){
 	int result = feature(7);
-	printf("feature=%i\n", result);
+	LOG("INFO", "feature=%i\n", result);
 	return 0;
 }
