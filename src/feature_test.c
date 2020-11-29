858e0c3a26ff70fe5b031bfdd26eb4ce0f5221b9
Felix Quehl <Felix.Quehl@icloud.com>
2020-11-29 20:35:26 +0100

    minor changes

--- src/feature_test.c
+++ src/feature_test.c
@@ -4,6 +4,7 @@
 
 int feature_test(void)
 {
+
 	int errors;
 	int temp;
 
@@ -12,23 +13,24 @@ int feature_test(void)
 	temp = feature(7);
 	if (temp != 14)
 	{
-		printf("errors: result for 7 should be 14 and not %i\n", temp);
+		printf("error: result for 7 should be 14 and not %i\n", temp);
 		errors++;
 	}
 
 	temp = feature(0);
 	if (temp != 0)
 	{
-		printf("errors: result for 0 should be 0 and not %i\n", temp);
+		printf("error: result for 0 should be 0 and not %i\n", temp);
 		errors++;
 	}
 
 	temp = feature(-1);
 	if (temp != -2)
 	{
-		printf("errors: result for -1 should be -2 and not %i\n", temp);
+		printf("error: result for -1 should be -2 and not %i\n", temp);
 		errors++;
 	}
 
 	return errors;
+
 }
